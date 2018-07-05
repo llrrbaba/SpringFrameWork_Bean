273f1e42f45226439c59086dca7b5093b0d39d5c
apple <rocker_pg@163.com>
2018-07-05 20:03:49 +0800

    初始提交

---
+++ src/main/java/cn/rocker/springframeworkbean/SpringFrameworkBeanApplication.java
@@ -0,0 +1,12 @@
+package cn.rocker.springframeworkbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkBeanApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/rocker/springframeworkbean/SpringFrameworkBeanApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.rocker.springframeworkbean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFrameworkBeanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
